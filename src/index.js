fbd8e7927ea441788018552639608d73bb5ebd46
Alena Koryavaya <elena.koryavaya@gmail.com>
2022-07-23 22:32:51 +0300

    update index.js (task completed)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let numIntoStr = String(n);
+
+    if (numIntoStr.startsWith("-")) {
+        numIntoStr = numIntoStr.slice(1);
+    }
+
+    if (numIntoStr.endsWith("0")) {
+        numIntoStr = numIntoStr.slice(0, -1);
+    }
+    return numIntoStr.split("").reverse().join("");
+};
